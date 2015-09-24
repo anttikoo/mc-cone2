d817730a9d6497c0d8fe3c2e987246e7d37ddbbf
Antti Kurronen <antti.kurronen@gmail.com>
2015-09-24 14:33:11 +0300

    adding comments

--- MCCone/src/gui/ContentPane.java
+++ MCCone/src/gui/ContentPane.java
@@ -9,13 +9,19 @@ import java.util.logging.Logger;
 
 import javax.swing.JPanel;
 
+/**
+ * Class ContentPane is used as ContentPane for dialogs to get black dimming outside of window.
+ * @author Antti Kurronen
+ *
+ */
 public class ContentPane extends JPanel{
 	private final static Logger LOGGER = Logger.getLogger("MCCLogger");
 
-	 public ContentPane() {
-
+	 /**
+	 * Class constructor
+	 */
+	public ContentPane() {
 	        setOpaque(false);
-
 	    }
 
 	    @Override
@@ -29,16 +35,12 @@ public class ContentPane extends JPanel{
 				Graphics2D g2d = (Graphics2D) g.create();
 
 				// 50% transparent Alpha
-     //   g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.7F));		// THIS WORKING ONLY IN WINDOWS
-	//			g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_IN, 0.7F));			// THIS WORKING IN LINUX
-				g2d.setComposite(AlphaComposite.getInstance(SharedVariables.usedDimmingMode, 0.7F));			// THIS WORKING IN LINUX
+				//  g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.7F));		// THIS WORKING ONLY IN WINDOWS
+				//	g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_IN, 0.7F));			// THIS WORKING IN LINUX
+				g2d.setComposite(AlphaComposite.getInstance(SharedVariables.usedDimmingMode, 0.7F)); // use SharedVarible
 			//	g2d.setComposite(AlphaComposite.SrcIn.derive(0.8f));			// THIS WORKING IN LINUX
-
-			
-				
-				
+	
 				g2d.setColor(getBackground());
-			//	LOGGER.fine("bachground "+this.getBackground());
 				g2d.fill(getBounds());
 
 				g2d.dispose();
