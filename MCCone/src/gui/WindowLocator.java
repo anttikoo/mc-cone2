be2e93158cc8c78954408964c1d6781d202e3bc1
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-03 14:03:32 +0200

    cleaning code

--- MCCone/src/gui/WindowLocator.java
+++ MCCone/src/gui/WindowLocator.java
@@ -3,11 +3,8 @@ package gui;
 import java.awt.Component;
 import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
-
 import javax.swing.JComponent;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
-
 import information.ID;
 import information.SharedVariables;
 
@@ -30,39 +27,27 @@ public class WindowLocator {
 			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 			Rectangle windowBounds = ge.getMaximumWindowBounds();
 			
-			int windowLeftX = windowBounds.x;	
-			/*	unused
-		   	int windowRightX=windowBounds.x+windowBounds.width;	 
-			int windowUpY=windowBounds.y;
-			int windowDownY=windowBounds.y+windowBounds.height;
-			*/
+			int windowLeftX = windowBounds.x;				
 			int x=parentRectangle.x;		
 			int y= parentRectangle.y;
 			int width=parentRectangle.width;
 			int height=parentRectangle.height;
-			// GUI over window
-
 			
+			// GUI over window		
 			int guiX= parentRectangle.x;
-		//	int guiY= parentRectangle.y;
 			int guiWidth= parentRectangle.width;
-		//	int guiHeight=this.getHeight();
-			
+				
 			if(guiX<windowLeftX){
 				x=windowLeftX;
 				width = guiWidth-(windowLeftX-guiX);
 			}
 			
-			
-			
 			return new Rectangle(x,y,width,height);
-			
-			
+					
 		}
 		
-		// normally 
+		// normally returned only bounds of comp
 		return parentRectangle;
-
 
 	}
 
