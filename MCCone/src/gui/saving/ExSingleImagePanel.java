5fbf883e2723eedc3f59e62d4ba11e8dca04d6e6
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-04 13:16:38 +0200

    commenting

--- MCCone/src/gui/saving/ExSingleImagePanel.java
+++ MCCone/src/gui/saving/ExSingleImagePanel.java
@@ -2,19 +2,34 @@ package gui.saving;
 
 import javax.swing.JPanel;
 
-import information.ID;
 import information.ImageLayer;
 
+/**
+ * The Class ExSingleImagePanel. Export Single Image Panel.
+ */
 public class ExSingleImagePanel extends SingleImagePanel{
 
+	/**
+	 * Instantiates a new ExSingleImagePanel.
+	 *
+	 * @param imageLayer the ImageLayer
+	 * @param saverDialog the saver dialog
+	 */
 	public ExSingleImagePanel(ImageLayer imageLayer, SaverDialog saverDialog) {
 		super(imageLayer, saverDialog);
-		// TODO Auto-generated constructor stub
+		
 	}
+	
+	/* (non-Javadoc)
+	 * @see gui.saving.SingleImagePanel#initFilePathPanelWithLabel()
+	 */
 	protected JPanel initFilePathPanelWithLabel() throws Exception{
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see gui.saving.SingleImagePanel#setProperFileForMarkings(java.lang.String, boolean, int)
+	 */
 	public void setProperFileForMarkings(String properFileForMarkings, boolean showMessage, int fileValidityID) {
 		// do nothing
 	}
