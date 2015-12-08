a3cfb855a9e740ff96993dc5fc3581a119e73236
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-08 08:51:52 +0200

    commenting and cleaning

--- MCCone/src/gui/saving/image/ExImaSingleImagePanel.java
+++ MCCone/src/gui/saving/image/ExImaSingleImagePanel.java
@@ -1,26 +1,39 @@
 package gui.saving.image;
 
 import java.awt.Component;
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Iterator;
-
-import javax.swing.JPanel;
-
 import gui.saving.SaverDialog;
 import gui.saving.SingleImagePanel;
 import gui.saving.SingleMarkingPanel;
-import information.ID;
 import information.ImageLayer;
 import information.MarkingLayer;
 
+/**
+ * The Class ExImaSingleImagePanel. Shows information of single ImageLayer and its MarkingLayers when exporting ImageLayers.
+ */
 public class ExImaSingleImagePanel extends SingleImagePanel{
 
+	/**
+	 * Instantiates a new Panel.
+	 *
+	 * @param imageLayer the image layer
+	 * @param saverDialog the saver dialog
+	 */
 	public ExImaSingleImagePanel(ImageLayer imageLayer, SaverDialog saverDialog) {
 		super(imageLayer, saverDialog);
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see gui.saving.SingleImagePanel#createSingleMarkingPanel(information.MarkingLayer)
+	 */
+	protected ExImaSingleMarkingPanel createSingleMarkingPanel(MarkingLayer markingLayer) throws Exception{
+		return new ExImaSingleMarkingPanel(markingLayer);
+	}
+
+	/* (non-Javadoc)
+	 * @see gui.saving.SingleImagePanel#getAllSelectedMarkingLayerIDs()
+	 */
 	public ArrayList<Integer> getAllSelectedMarkingLayerIDs(){
 		ArrayList<Integer> selectedMarkingLayerIDs=new ArrayList<Integer>();
 		Component[] sMarkingList= markingTableJPanel.getComponents();
@@ -30,15 +43,14 @@ public class ExImaSingleImagePanel extends SingleImagePanel{
 				if(smp.isSelected()){
 					selectedMarkingLayerIDs.add(smp.getMarkingLayer().getLayerID());
 				}
-
-
 			}
-
 		}
 		return selectedMarkingLayerIDs;
-
 	}
 
+	/* (non-Javadoc)
+	 * @see gui.saving.SingleImagePanel#getAllSelectedMarkingLayers()
+	 */
 	public ArrayList<MarkingLayer> getAllSelectedMarkingLayers(){
 		ArrayList<MarkingLayer> selectedMarkingLayers=new ArrayList<MarkingLayer>();
 		Component[] sMarkingList= markingTableJPanel.getComponents();
@@ -49,34 +61,14 @@ public class ExImaSingleImagePanel extends SingleImagePanel{
 					selectedMarkingLayers.add(smp.getMarkingLayer());
 				}
 			}
-
 		}
 		return selectedMarkingLayers;
 
 	}
 
-	public void updateGridDrawing(){
-
-		Component[] sMarkingList= markingTableJPanel.getComponents();
-		if(sMarkingList != null && sMarkingList.length>0){
-			for(int i=0;i<sMarkingList.length;i++){
-				ExImaSingleMarkingPanel esmp= (ExImaSingleMarkingPanel)sMarkingList[i];
-				if(esmp.isSelected()){
-					if(esmp.isDrawCheckBoxSelected()){
-						MarkingLayer ml = this.imageLayer.getMarkingLayer(esmp.getMarkingLayerID());
-						if(ml != null){
-							ml.setDrawGridToImage(true);
-						}
-					}
-				}
-			}
-
-		}
-
-	}
-
-
-
+	/* (non-Javadoc)
+	 * @see gui.saving.SingleImagePanel#initProperFilePathForSaving(information.ImageLayer)
+	 */
 	protected String initProperFilePathForSaving(ImageLayer iLayer){
 		if(iLayer != null){
 				if(iLayer.getExportImagePath() != null && iLayer.getExportImagePath().length()>0)
@@ -91,11 +83,27 @@ public class ExImaSingleImagePanel extends SingleImagePanel{
 
 	}
 	
+	/**
+	 * Updates MarkingLayer grid drawing property to true if user has selected the Grid to be drawn on export image.
+	 */
+	public void updateGridDrawing(){
 
+		Component[] sMarkingList= markingTableJPanel.getComponents();
+		if(sMarkingList != null && sMarkingList.length>0){
+			for(int i=0;i<sMarkingList.length;i++){
+				ExImaSingleMarkingPanel esmp= (ExImaSingleMarkingPanel)sMarkingList[i];
+				if(esmp.isSelected()){
+					if(esmp.isDrawCheckBoxSelected()){ // user has selected the Grid to be drawn on image.
+						MarkingLayer ml = this.imageLayer.getMarkingLayer(esmp.getMarkingLayerID());
+						if(ml != null){
+							ml.setDrawGridToImage(true);
+						}
+					}
+				}
+			}
 
+		}
 
-	protected ExImaSingleMarkingPanel createSingleMarkingPanel(MarkingLayer markingLayer) throws Exception{
-		return new ExImaSingleMarkingPanel(markingLayer);
 	}
 
 }
