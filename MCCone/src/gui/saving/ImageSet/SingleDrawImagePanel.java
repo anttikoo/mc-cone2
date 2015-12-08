fadec64087fdf8382d134297bb0a2a1a3e80a450
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-08 17:56:59 +0200

    commenting

--- MCCone/src/gui/saving/ImageSet/SingleDrawImagePanel.java
+++ MCCone/src/gui/saving/ImageSet/SingleDrawImagePanel.java
@@ -1,45 +1,40 @@
 package gui.saving.ImageSet;
 
 import gui.Color_schema;
-import information.ImageLayer;
-
 import java.awt.BorderLayout;
-import java.awt.ComponentOrientation;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.GridBagLayout;
-import java.awt.Point;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
-
-import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
 import managers.TaskManager;
 
+/**
+ * The Class SingleDrawImagePanel contains information of ImageLayers and MarkingLayers of them. 
+ * User can select which ImageLayers and MarkingLayers are drawn to exported images.
+ */
 public class SingleDrawImagePanel extends JPanel{
 	private SingleImage singleImage;
 	private int[] gridPosition=null; // row, column
 	private String title;
-
-
 	private Font font;
 	private JPanel titlePanel;
-	private int titlePanelHeight=30;
 	private BufferedImage image;
 	private TaskManager taskManager;
-//	private JLabel titleLabel;
 	private JTextField titleField;
 	private Dimension panelDimension;
 
 
+	/**
+	 * Instantiates a new SingleDrawImagePanel.
+	 *
+	 * @param bi the bi
+	 * @param title the title
+	 * @param taskManager the task manager
+	 * @param font the font
+	 */
 	public SingleDrawImagePanel(BufferedImage bi, String title,TaskManager taskManager, Font font ){
 		this.setOpaque(true);
 		this.taskManager=taskManager;
@@ -47,123 +42,130 @@ public class SingleDrawImagePanel extends JPanel{
 		this.title=title;
 		this.setImage(bi);
 		this.font=font;
-
-
 		initPanel();
 		this.revalidate();
 		this.repaint();
 
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.Component#getFont()
+	 */
+	public Font getFont() {
+		return font;
+	}
+
+	/**
+	 * Returns the grid position.
+	 *
+	 * @return the grid position
+	 */
+	public int[] getGridPosition(){
+		return this.gridPosition;
+	}
+
+	/**
+	 * Returns the image.
+	 *
+	 * @return the image
+	 */
+	public BufferedImage getImage() {
+		return image;
+	}
+
+	/**
+	 * Returns the panel size.
+	 *
+	 * @return the panel size
+	 */
+	public Dimension getPanelSize(){
+		return this.panelDimension;
+	}
+
+	/**
+	 * Returns the scaled image.
+	 *
+	 * @param destination the destination
+	 * @return the scaled image
+	 */
+	public BufferedImage getScaledImage(Dimension destination){
+		return this.singleImage.getScaledImage(destination);
+	}
+
+	/**
+	 * Returns the scaled image dimension.
+	 *
+	 * @return the scaled image dimension
+	 */
+	public Dimension getScaledImageDimension(){
+		return this.singleImage.getScaledImageDimension();
+	}
+
+	/**
+	 * Returns the title.
+	 *
+	 * @return the title
+	 */
+	public String getTitle() {
+		return this.titleField.getText();
+	}
+
+	/**
+	 * Returns the title panel height.
+	 *
+	 * @return the title panel height
+	 */
+	public int getTitlePanelHeight(){
+		return this.titlePanel.getPreferredSize().height;
+	}
+
+	/**
+	 * Returns the title width.
+	 *
+	 * @param f the Font
+	 * @return the title width
+	 */
+	private int getTitleWidth(Font f){
+		return this.titleField.getFontMetrics(f).stringWidth(this.title);
+	}
+
+	/**
+	 * Initializes the panel.
+	 */
 	private void initPanel(){
 
 		this.setLayout(new BorderLayout());
-	//	this.setLayout(null);
-	//	this.setBorder(BorderFactory.createLineBorder(Color_schema.color_white_230, 2));
 		this.setBackground(Color_schema.white_230);
 		titlePanel = new JPanel();
 		this.titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.PAGE_AXIS));
 		this.titlePanel.setBackground(Color_schema.white_230);
-	//	titleLabel = new JLabel(this.title);
-	//	titleLabel.setFont(font);
-	//	titleLabel.setForeground(Color_schema.color_white_230);
-	//	this.titlePanel.add(titleLabel);
-
 		this.titleField=new JTextField(this.title);
-		//this.titleField.setText(this.title);
 		this.titleField.setFont(font);
 		this.titleField.setHorizontalAlignment(JLabel.LEFT);
-
-
 		this.titleField.setBackground(Color_schema.white_230);
 		this.titleField.setForeground(Color_schema.dark_20);
 		this.titleField.setMinimumSize(new Dimension(300,font.getSize()+6));
 		this.titleField.setBorder(null);
 		this.titlePanel.add(this.titleField);
-
-/*
-		JPanel imagePanel=new JPanel();
-		imagePanel.setLayout(new BorderLayout());
-		imagePanel.setBackground(Color_schema.color_dark_40_bg);
-		if(image != null){
-			JLabel backLabel=new JLabel();
-			ImageIcon icon= new ImageIcon(image);
-			backLabel.setIcon(icon);
-		//	backLabel.setLayout(new BorderLayout());
-			imagePanel.add(backLabel, BorderLayout.CENTER);
-		}
-		*/
-	/*	JPanel singleImagePanel = new JPanel();
-		singleImagePanel.setLayout(new BoxLayout(singleImagePanel, BoxLayout.X_AXIS));
-		singleImagePanel.setBackground(Color_schema.color_orange_bright);
-		singleImagePanel.add(this.singleImage);
-*/
 		this.add(titlePanel, BorderLayout.PAGE_START);
 		this.add(this.singleImage, BorderLayout.CENTER);
-	//	this.add(titlePanel);
-	//	this.add(this.singleImage);
-
 	}
 
-	public Font getFont() {
-		return font;
-	}
-
-	public void updateFont(Font f) {
-		if(this.titlePanel.getPreferredSize().width >0)
-		while(this.titlePanel.getPreferredSize().width < getTitleWidth(f)){
-			f=new Font(f.getFontName(),f.getStyle(),f.getSize()-1);
-		}
-		this.font = f;
-		this.titleField.setFont(this.font);
-		this.titleField.repaint();
-
-	}
-
-	public BufferedImage getScaledImage(Dimension destination){
-		return this.singleImage.getScaledImage(destination);
-	}
-
-	public String getTitle() {
-		return this.titleField.getText();
-	}
-
-	public void setGridPosition(int r, int c){
-		this.gridPosition=new int[]{r,c};
-	}
-
-	public void setGridPosition(int[] rc){
-		this.gridPosition=rc;
-	}
-
-	public int[] getGridPosition(){
-		return this.gridPosition;
-	}
-
-	public void setGridPositionNull(){
-		this.gridPosition=null;
-	}
-
-	private int getTitleWidth(Font f){
-		return this.titleField.getFontMetrics(f).stringWidth(this.title);
-	}
-
-	public Dimension getScaledImageDimension(){
-		return this.singleImage.getScaledImageDimension();
-	}
-
-
-
+	/**
+	 * Sets the dimension for the single ImagePanel drawn on ImageSet window.
+	 *
+	 * @param dim the dim
+	 * @param titlePanelHeight the title panel height
+	 * @return the dimension
+	 * @throws Exception the exception
+	 */
 	public Dimension setDimension(Dimension dim, int titlePanelHeight) throws Exception{
-		//int titlePanelHeight=this.font.getSize()+6;
 		Dimension imageDimension=this.singleImage.setImageDimension(new Dimension(dim.width,dim.height-titlePanelHeight));
-
 		panelDimension = new Dimension(imageDimension.width, imageDimension.height+titlePanelHeight);
 
 		this.setMaximumSize(panelDimension);
 		this.setPreferredSize(panelDimension);
 		this.setMinimumSize(panelDimension);
-
 		this.titlePanel.setMaximumSize(new Dimension(panelDimension.width,titlePanelHeight));
 		this.titlePanel.setMinimumSize(new Dimension(panelDimension.width,titlePanelHeight));
 		this.titlePanel.setPreferredSize(new Dimension(panelDimension.width,titlePanelHeight));
@@ -175,30 +177,55 @@ public class SingleDrawImagePanel extends JPanel{
 
 	}
 
-	public BufferedImage getImage() {
-		return image;
+	/**
+	 * Sets the grid position.
+	 *
+	 * @param r the r
+	 * @param c the c
+	 */
+	public void setGridPosition(int r, int c){
+		this.gridPosition=new int[]{r,c};
 	}
 
-	public int getTitlePanelHeight(){
-		return this.titlePanel.getPreferredSize().height;
+	/**
+	 * Sets the selected grid position.
+	 *
+	 * @param rc the new grid position
+	 */
+	public void setGridPosition(int[] rc){
+		this.gridPosition=rc;
 	}
 
+	/**
+	 * Sets the grid position null.
+	 */
+	public void setGridPositionNull(){
+		this.gridPosition=null;
+	}
+
+	/**
+	 * Sets the image.
+	 *
+	 * @param image the new image
+	 */
 	public void setImage(BufferedImage image) {
 		this.image = image;
 	}
 
-	public Dimension getPanelSize(){
-		return this.panelDimension;
+	/**
+	 * Update font of titles.
+	 *
+	 * @param f the Font
+	 */
+	public void updateFont(Font f) {
+		if(this.titlePanel.getPreferredSize().width >0)
+		while(this.titlePanel.getPreferredSize().width < getTitleWidth(f)){
+			f=new Font(f.getFontName(),f.getStyle(),f.getSize()-1);
+		}
+		this.font = f;
+		this.titleField.setFont(this.font);
+		this.titleField.repaint();
+
 	}
-
-
-
-
-
-
-
-
-
-
 
 }
