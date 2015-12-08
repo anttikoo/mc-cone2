92c62443d5fac8673e233ae174a724ac9d573640
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-08 08:46:00 +0200

    adding comments

--- MCCone/src/gui/saving/SMSingleImagePanel.java
+++ MCCone/src/gui/saving/SMSingleImagePanel.java
@@ -1,16 +1,27 @@
 package gui.saving;
 
 import javax.swing.JPanel;
-
 import information.ImageLayer;
 
+/**
+ * The Class SMSingleImagePanel extends SingleImagePanel for showing information when saving markings.
+ */
 public class SMSingleImagePanel extends SingleImagePanel{
 
+	/**
+	 * Instantiates a new SM single image panel.
+	 *
+	 * @param imageLayer the image layer
+	 * @param saverDialog the saver dialog
+	 */
 	public SMSingleImagePanel(ImageLayer imageLayer, SaverDialog saverDialog) {
 		super(imageLayer, saverDialog);
-		// TODO Auto-generated constructor stub
+		
 	}
 
+	/* (non-Javadoc)
+	 * @see gui.saving.SingleImagePanel#initFilePathPanelWithLabel()
+	 */
 	protected JPanel initFilePathPanelWithLabel() throws Exception{
 		return initFilePathPanel("Browse xml-file");
 	}
--- MCCone/src/gui/saving/SingleMarkingPanel.java
+++ MCCone/src/gui/saving/SingleMarkingPanel.java
@@ -20,134 +20,153 @@ import operators.CheckBoxIcon;
  * The Class SingleMarkingPanel. Contains name of MarkingLayer, checkboxes to select it and grid.
  */
 public class SingleMarkingPanel extends JPanel{
-
-		private JCheckBox saveCheckBox;
-		protected MarkingLayer mLayer;
-		private JPanel markingLabelPanel;
-		private JPanel gridDrawingSelection;
-
+	private JCheckBox saveCheckBox;
+	protected MarkingLayer mLayer;
+	private JPanel markingLabelPanel;
+	private JPanel gridDrawingSelection;
 
 
-		/**
-		 * Instantiates a new single marking panel.
-		 *
-		 * @param mLayer the MarkingLayer
-		 */
-		public SingleMarkingPanel(MarkingLayer mLayer){
-			this.mLayer=mLayer;
+	/**
+	 * Instantiates a new single marking panel.
+	 *
+	 * @param mLayer the MarkingLayer
+	 */
+	public SingleMarkingPanel(MarkingLayer mLayer){
+		this.mLayer=mLayer;
 
-			try {
-				this.setOpaque(true);
-				this.setMaximumSize(new Dimension(2000,40));
-				this.setPreferredSize(new Dimension(400,40));
-				this.setMinimumSize(new Dimension(200,40));
-				this.setBackground(Color_schema.dark_35);
-				this.setBorder(BorderFactory.createLineBorder(Color_schema.dark_50, 1));
-				this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
-				Icon checkBoxIcon=new CheckBoxIcon();
-				// checkBox for selecting
-				saveCheckBox=new JCheckBox(checkBoxIcon);		
-				saveCheckBox.setSelected(true);
-				saveCheckBox.setBackground(Color_schema.dark_35);
-				saveCheckBox.setMaximumSize(new Dimension(25,25));
-				saveCheckBox.setPreferredSize(new Dimension(25,25));
-				saveCheckBox.setMinimumSize(new Dimension(25,25));
+		try {
+			this.setOpaque(true);
+			this.setMaximumSize(new Dimension(2000,40));
+			this.setPreferredSize(new Dimension(400,40));
+			this.setMinimumSize(new Dimension(200,40));
+			this.setBackground(Color_schema.dark_35);
+			this.setBorder(BorderFactory.createLineBorder(Color_schema.dark_50, 1));
+			this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+			Icon checkBoxIcon=new CheckBoxIcon();
+			// checkBox for selecting
+			saveCheckBox=new JCheckBox(checkBoxIcon);		
+			saveCheckBox.setSelected(true);
+			saveCheckBox.setBackground(Color_schema.dark_35);
+			saveCheckBox.setMaximumSize(new Dimension(25,25));
+			saveCheckBox.setPreferredSize(new Dimension(25,25));
+			saveCheckBox.setMinimumSize(new Dimension(25,25));
 
-				// marking title
-				JLabel markingLabel = new JLabel(getMarkingName());		
-				markingLabelPanel = new JPanel();
-				markingLabelPanel.setLayout(new BorderLayout());
-				markingLabelPanel.setMaximumSize(new Dimension(2000, 40));
-				markingLabel.setFont(Fonts.b15);
-				markingLabel.setForeground(Color_schema.white_230);
-				int markinglabelwidth=markingLabel.getFontMetrics(Fonts.b15).stringWidth(markingLabel.getText());
+			// marking title
+			JLabel markingLabel = new JLabel(getMarkingName());		
+			markingLabelPanel = new JPanel();
+			markingLabelPanel.setLayout(new BorderLayout());
+			markingLabelPanel.setMaximumSize(new Dimension(2000, 40));
+			markingLabel.setFont(Fonts.b15);
+			markingLabel.setForeground(Color_schema.white_230);
+			int markinglabelwidth=markingLabel.getFontMetrics(Fonts.b15).stringWidth(markingLabel.getText());
 
-				markingLabelPanel.setPreferredSize(new Dimension(markinglabelwidth+50,40));
-				markingLabelPanel.add(markingLabel);
-				markingLabelPanel.setBackground(Color_schema.dark_35);
-				gridDrawingSelection = initDrawGridCheckBoxPanel();
+			markingLabelPanel.setPreferredSize(new Dimension(markinglabelwidth+50,40));
+			markingLabelPanel.add(markingLabel);
+			markingLabelPanel.setBackground(Color_schema.dark_35);
+			gridDrawingSelection = initDrawGridCheckBoxPanel();
 
-				this.add(Box.createRigidArea(new Dimension(30,0)));
-				this.add(saveCheckBox);
-				this.add(Box.createRigidArea(new Dimension(5,0)));
-				this.add(markingLabelPanel);
-				if(gridDrawingSelection != null){
-					this.add(Box.createHorizontalGlue());
-					this.add(gridDrawingSelection);
-				}
-			
-			} catch (Exception e) {
+			this.add(Box.createRigidArea(new Dimension(30,0)));
+			this.add(saveCheckBox);
+			this.add(Box.createRigidArea(new Dimension(5,0)));
+			this.add(markingLabelPanel);
+			if(gridDrawingSelection != null){
+				this.add(Box.createHorizontalGlue());
+				this.add(gridDrawingSelection);
+			}
+		
+		} catch (Exception e) {
 
-				e.printStackTrace();
+			e.printStackTrace();
 		}
 
+	}
+
+	/**
+	 * Returns the MarkingLayer.
+	 *
+	 * @return the marking layer
+	 */
+	public MarkingLayer getMarkingLayer() {
+		return mLayer;
+	}
+
+	/**
+	 * Returns the ID of MarkingLayer
+	 *
+	 * @return the marking layer id
+	 */
+	public int getMarkingLayerID(){
+		return this.mLayer.getLayerID();
+	}
+
+	/**
+	 * Returns the name of MarkingLayer
+	 *
+	 * @return the marking name
+	 */
+	public String getMarkingName() {
+		return this.mLayer.getLayerName();
+	}
+	
+	/**
+	 * Initializes the JPanel for Grid CheckBox.
+	 *
+	 * @return the JPanel
+	 */
+	protected JPanel initDrawGridCheckBoxPanel(){
+		return null;
+	}
+
+	/**
+	 * Checks if is CheckBox selected.
+	 *
+	 * @return true, if is selected
+	 */
+	public boolean isSelected(){
+		return this.saveCheckBox.isSelected();
+	}
+
+
+
+	/**
+	 * Sets the background color for showing successfull saving (dark green).
+	 *
+	 * @param savedSuccessfully the new background color by successfull saving
+	 */
+	public void setBGColorBySuccessfullSaving(boolean savedSuccessfully){
+		if(savedSuccessfully){
+			this.setBackground(Color_schema.darkest_green);
+			this.saveCheckBox.setBackground(Color_schema.darkest_green);
+			this.markingLabelPanel.setBackground(Color_schema.darkest_green);
 		}
-
-		/**
-		 * Initializes the JPanel for Grid CheckBox.
-		 *
-		 * @return the JPanel
-		 */
-		protected JPanel initDrawGridCheckBoxPanel(){
-			return null;
+		else{
+		this.setBackground(Color_schema.dark_35);
+		this.saveCheckBox.setBackground(Color_schema.dark_35);
+		this.markingLabelPanel.setBackground(Color_schema.dark_35);
 		}
+	}
 
-			public boolean isSelected(){
-				return this.saveCheckBox.isSelected();
-			}
-
-			public void setSelected(boolean selected){
-				this.saveCheckBox.setSelected(selected);
-				this.repaint();
-			}
-			
-			public void setCheckBoxEnableState(boolean state){			
-				this.saveCheckBox.setEnabled(state);
-				this.repaint();
-			}
-
-			public int getMarkingLayerID(){
-				return this.mLayer.getLayerID();
-			}
+	/**
+	 * Sets the CheckBox enable state (enabled / unenabled)
+	 *
+	 * @param state the new CheckBox state
+	 */
+	public void setCheckBoxEnableState(boolean state){			
+		this.saveCheckBox.setEnabled(state);
+		this.repaint();
+	}
 
 
 
-			public String getMarkingName() {
-				return this.mLayer.getLayerName();
-			}
+	/**
+	 * Sets the CheckBox selected.
+	 *
+	 * @param selected the new selected
+	 */
+	public void setSelected(boolean selected){
+		this.saveCheckBox.setSelected(selected);
+		this.repaint();
+	}
+	
 
-			public MarkingLayer getMarkingLayer() {
-				return mLayer;
-			}
-
-
-
-			public void setBGColorBySuccessfullSaving(boolean savedSuccessfully){
-				if(savedSuccessfully){
-					this.setBackground(Color_schema.darkest_green);
-					this.saveCheckBox.setBackground(Color_schema.darkest_green);
-					this.markingLabelPanel.setBackground(Color_schema.darkest_green);
-					
-
-				}
-				else{
-				this.setBackground(Color_schema.dark_35);
-				this.saveCheckBox.setBackground(Color_schema.dark_35);
-				this.markingLabelPanel.setBackground(Color_schema.dark_35);
-				}
-			}
-		/*
-			public void setMarkingName(String markingName) {
-				this.markingName = markingName;
-			}
-
-			public int getImageLayerID() {
-				return iLayerID;
-			}
-
-			public void setImageLayerID(int iLayerID) {
-				this.iLayerID = iLayerID;
-			}
-		*/
-
-		}
+}
