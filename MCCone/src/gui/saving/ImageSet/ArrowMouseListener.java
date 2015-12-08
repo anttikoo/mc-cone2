a56bb686c9b85707a1173acae73f76f4877dd753
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-08 16:12:50 +0200

    commenting

--- MCCone/src/gui/saving/ImageSet/ArrowMouseListener.java
+++ MCCone/src/gui/saving/ImageSet/ArrowMouseListener.java
@@ -2,20 +2,31 @@ package gui.saving.ImageSet;
 
 import gui.Color_schema;
 import information.SharedVariables;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.logging.Logger;
-
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.Timer;
 import javax.swing.text.DocumentFilter;
 import javax.swing.text.PlainDocument;
 
+/**
+ * The listener interface for receiving arrowMouse events.
+ * The class that is interested in processing a arrowMouse
+ * event implements this interface, and the object created
+ * with that class is registered with a component using the
+ * component's <code>addArrowMouseListener<code> method. When
+ * the arrowMouse event occurs, that object's appropriate
+ * method is invoked.
+ *
+ * @see ArrowMouseEvent
+ */
 public class ArrowMouseListener implements MouseListener {
+	private final static Logger LOGGER = Logger.getLogger("MCCLogger");
+	public static double scalingFactor=0;
 	public final String widthUp = "WIDTH_UP";
 	public final String widthDown = "WIDTH_DOWN";
 	public final String heighthUp = "HEIGHT_UP";
@@ -30,12 +41,16 @@ public class ArrowMouseListener implements MouseListener {
 	private boolean changingValuesON=false;
 	private int valueChange=0;
 	private int maxValueChange=30;
-	private final static Logger LOGGER = Logger.getLogger("MCCLogger");
 	private int maximumImageSize=SharedVariables.IMAGESET_EXPORT_MAX_RESOLUTION;
-	public static double scalingFactor=0;
 
 
 
+	/**
+	 * Instantiates a new ArrowMouseListener.
+	 *
+	 * @param wf TextField of width for exported image
+	 * @param hf TextField of height for exported image
+	 */
 	public ArrowMouseListener(JTextField wf, JTextField hf){
 		this.widthField=wf;
 		this.heightField=hf;
@@ -43,10 +58,9 @@ public class ArrowMouseListener implements MouseListener {
 
 	}
 
-	public void setScalingFactor(double factor){
-		ArrowMouseListener.scalingFactor=factor;
-	}
-
+	/**
+	 * Initializes the Timers for pressing keys.
+	 */
 	private void initPressingTimers(){
 		this.pressingWidthUPTimer=new Timer(50, new ActionListener() {
 			@Override
@@ -89,6 +103,10 @@ public class ArrowMouseListener implements MouseListener {
 
 
 	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseClicked(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		stopTimers();
@@ -110,7 +128,26 @@ public class ArrowMouseListener implements MouseListener {
 		}
 
 	}
+	
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseEntered(java.awt.event.MouseEvent)
+	 */
+	@Override
+	public void mouseEntered(MouseEvent e) {
 
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseExited(java.awt.event.MouseEvent)
+	 */
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mousePressed(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mousePressed(MouseEvent e) {
 		if(!this.multiplierTimer.isRunning())
@@ -136,30 +173,48 @@ public class ArrowMouseListener implements MouseListener {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.MouseListener#mouseReleased(java.awt.event.MouseEvent)
+	 */
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		stopTimers();
 
 	}
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
+	/**
+	 * Sets the scaling factor used for calculating width from image height and height from image width.
+	 *
+	 * @param factor the new scaling factor
+	 */
+	public void setScalingFactor(double factor){
+		ArrowMouseListener.scalingFactor=factor;
 	}
 
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
+	/**
+	 * Stops the timers.
+	 */
+	private void stopTimers(){
+		if(this.multiplierTimer.isRunning()){
+			this.multiplierTimer.stop();
+			this.valueChange=0;
+		}
 
-	}
 
-	public void updateResolutions(){
-		updateField(0,this.widthField, this.heightField, false);
+		if(this.pressingWidthUPTimer.isRunning())
+			this.pressingWidthUPTimer.stop();
+
+		if(this.pressingWidthDownTimer.isRunning())
+			this.pressingWidthDownTimer.stop();
+
+		if(this.pressingHeightUPTimer.isRunning())
+			this.pressingHeightUPTimer.stop();
+
+		if(this.pressingHeightDownTimer.isRunning())
+			this.pressingHeightDownTimer.stop();
 	}
 	
 	
-
 
 	/**
 	 * Updates textfields of resolution for exported image. If given JTextField is widthField then the divide parameter has to be false and conversely.
@@ -207,16 +262,12 @@ public class ArrowMouseListener implements MouseListener {
 							field_calculated.setForeground(Color_schema.orange_dark);
 
 						}
-
-
-
 					}
 					else{
 
 						field_changed.setText(""+presentValue);
 						field_changed.setForeground(Color_schema.white_230);
 					}
-
 				}
 				else{
 					if(presentValue >maximumImageSize){
@@ -271,24 +322,11 @@ public class ArrowMouseListener implements MouseListener {
 
 
 
-	private void stopTimers(){
-		if(this.multiplierTimer.isRunning()){
-			this.multiplierTimer.stop();
-			this.valueChange=0;
-		}
-
-
-		if(this.pressingWidthUPTimer.isRunning())
-			this.pressingWidthUPTimer.stop();
-
-		if(this.pressingWidthDownTimer.isRunning())
-			this.pressingWidthDownTimer.stop();
-
-		if(this.pressingHeightUPTimer.isRunning())
-			this.pressingHeightUPTimer.stop();
-
-		if(this.pressingHeightDownTimer.isRunning())
-			this.pressingHeightDownTimer.stop();
+	/**
+	 * Updates JTextFields of width and height.
+	 */
+	public void updateResolutions(){
+		updateField(0,this.widthField, this.heightField, false);
 	}
 
 }
