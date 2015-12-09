99c5d160f448db34ee212c975f6154c5af81b6d5
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-09 13:56:15 +0200

    comments

--- MCCone/src/information/SharedVariables.java
+++ MCCone/src/information/SharedVariables.java
@@ -2,6 +2,9 @@ package information;
 
 import java.awt.AlphaComposite;
 
+/**
+ * The Class SharedVariables. Contains static variables.
+ */
 public class SharedVariables {
 	public static final int IMAGESET_EXPORT_MAX_RESOLUTION = 5000;
 	public static final int DISTANCE_TO_REMOVE = 20;
@@ -11,34 +14,59 @@ public class SharedVariables {
 	public static int transparencyModeOVER = AlphaComposite.SRC_OVER;
 	public static int transparencyModeOut= AlphaComposite.SRC_OUT;
 	public static int transparencyModeATOP= AlphaComposite.SRC_ATOP;
-
 	public static int usedDimmingMode=transparencyModeIN;
 
-
+	/**
+	 * Sets the transparencyIN.
+	 *
+	 * @param trm the new transparency in
+	 */
 	public static void setTransParencyIn(int trm){
 		transparencyModeIN=trm;
 	}
 
+	/**
+	 * Sets the transparency over.
+	 *
+	 * @param trm the new transparency over
+	 */
 	public static void setTransParencyOver(int trm){
 		transparencyModeOVER=trm;
 	}
 	
+	/**
+	 * Sets the used dimming mode to srcOver.
+	 */
 	public static void setUsedDimmingModeToSrcOver(){
 		usedDimmingMode=transparencyModeOVER;
 	}
 	
+	/**
+	 * Sets the used dimming mode to srcIn.
+	 */
 	public static void setUsedDimmingModeToSrcIn(){
 		usedDimmingMode=transparencyModeIN;
 	}
 	
+	/**
+	 * Sets the used dimming mode to srcOut.
+	 */
 	public static void setUsedDimmingModeToSrcOut(){
 		usedDimmingMode=transparencyModeOut;
 	}
 	
+	/**
+	 * Sets the used dimming mode to srcAtop.
+	 */
 	public static void setUsedDimmingModeToSrcAtop(){
 		usedDimmingMode=transparencyModeATOP;
 	}
 	
+	/**
+	 * Sets the ID of operation system.
+	 *
+	 * @param osID the new ID of operation system
+	 */
 	public static void setOS(int osID){
 		operationSystem=osID;
 	}
