fb46da0de1cf771d2dbb7b2c6fb451b21ed6c431
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-09 12:40:50 +0200

    commenting

--- MCCone/src/information/LayersOfPath.java
+++ MCCone/src/information/LayersOfPath.java
@@ -2,15 +2,20 @@ package information;
 
 import java.util.ArrayList;
 
+/**
+ * The Class LayersOfPath. Contains list of ImageLayers and file path where the data of ImageLayers are saved. 
+ */
 public class LayersOfPath {
 	private String xmlpath;
 	private ArrayList<ImageLayer> imageLayerList;
 	private int fileState;
 	
-	public int getFileState() {
-		return fileState;
-	}
-
+	/**
+	 * Instantiates a new LayersOfPath
+	 *
+	 * @param path the path
+	 * @param state the state
+	 */
 	public LayersOfPath(String path, int state){
 		this.setXmlpath(path);
 		this.imageLayerList=new ArrayList<ImageLayer>();
@@ -18,22 +23,56 @@ public class LayersOfPath {
 		
 	}
 
-	public ArrayList<ImageLayer> getImageLayerList() {
-		return imageLayerList;
-	}
-
-	public void setImageLayerList(ArrayList<ImageLayer> imageLayerList) {
-		this.imageLayerList = imageLayerList;
-	}
-	
+	/**
+	 * Adds the ImageLayer to list.
+	 *
+	 * @param imageLayer the ImageLayer
+	 */
 	public void addImageLayer(ImageLayer imageLayer) {
 		this.imageLayerList.add(imageLayer);
 	}
 
+	/**
+	 * Returns the file state.
+	 *
+	 * @return the file state
+	 */
+	public int getFileState() {
+		return fileState;
+	}
+
+	/**
+	 * Returns the list of ImageLayers.
+	 *
+	 * @return the list of ImageLayers
+	 */
+	public ArrayList<ImageLayer> getImageLayerList() {
+		return imageLayerList;
+	}
+	
+	/**
+	 * Returns the file path of xml-file.
+	 *
+	 * @return the xml-file path
+	 */
 	public String getXmlpath() {
 		return xmlpath;
 	}
 
+	/**
+	 * Sets the list of ImageLayers.
+	 *
+	 * @param imageLayerList the new list of ImageLayers
+	 */
+	public void setImageLayerList(ArrayList<ImageLayer> imageLayerList) {
+		this.imageLayerList = imageLayerList;
+	}
+
+	/**
+	 * Sets the xml-file path.
+	 *
+	 * @param xmlpath the new xml-file path
+	 */
 	public void setXmlpath(String xmlpath) {
 		this.xmlpath = xmlpath;
 	}
