ce2525576419edbbeb9924b9079ab3be992737a1
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-09 13:16:16 +0200

    commmenting

--- MCCone/src/information/PositionedImage.java
+++ MCCone/src/information/PositionedImage.java
@@ -3,32 +3,67 @@ package information;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 
+/**
+ * The Class PositionedImage. Contains image and its position at window.
+ */
 public class PositionedImage {
-private BufferedImage image = null;
-private Point position =null;
-
-public PositionedImage(BufferedImage im, Point p){
-	this.setImage(im);
-	this.setPosition(p);
-}
-public PositionedImage(BufferedImage im){
-	this.setImage(im);
-	this.setPosition(new Point(0,0));
-}
-
-public BufferedImage getImage() {
-	return image;
-}
-
-public void setImage(BufferedImage image) {
-	this.image = image;
-}
-
-public Point getPosition() {
-	return position;
-}
-
-public void setPosition(Point position) {
-	this.position = position;
-}
+	private BufferedImage image = null;
+	private Point position =null;
+	
+	/**
+	 * Instantiates a new positioned image.
+	 *
+	 * @param im the BufferedImage
+	 */
+	public PositionedImage(BufferedImage im){
+		this.setImage(im);
+		this.setPosition(new Point(0,0));
+	}
+	
+	/**
+	 * Instantiates a new positioned image.
+	 *
+	 * @param im the BufferedImage
+	 * @param p the Point
+	 */
+	public PositionedImage(BufferedImage im, Point p){
+		this.setImage(im);
+		this.setPosition(p);
+	}
+	
+	/**
+	 * Returns the image.
+	 *
+	 * @return the BufferedImage
+	 */
+	public BufferedImage getImage() {
+		return image;
+	}
+	
+	/**
+	 * Returns the position.
+	 *
+	 * @return the position Point
+	 */
+	public Point getPosition() {
+		return position;
+	}
+	
+	/**
+	 * Sets the image.
+	 *
+	 * @param image the new Bufferedimage
+	 */
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+	
+	/**
+	 * Sets the position.
+	 *
+	 * @param position the new position Point
+	 */
+	public void setPosition(Point position) {
+		this.position = position;
+	}
 }
