019bb74c84e8a275ea748d343e94e0337b8be7e9
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-09 13:13:27 +0200

    commenting

--- MCCone/src/information/PathCount.java
+++ MCCone/src/information/PathCount.java
@@ -1,28 +1,64 @@
 package information;
 
+/**
+ * The Class PathCount. Contains file path and amount how many times has saved to that path.
+ */
 public class PathCount {
 	private int count;
 	private String folderPath;
+	
+	
+	/**
+	 * Instantiates a new path count.
+	 *
+	 * @param folderPath the folder path
+	 */
 	public PathCount(String folderPath){
 		this.setCount(1);
 		this.setFolderPath(folderPath);
 	}
-	public String getFolderPath() {
-		return folderPath;
+	
+	/**
+	 * Adds the countings with one.
+	 */
+	public void addOne(){
+		this.count++;
 	}
-	public void setFolderPath(String folderPath) {
-		this.folderPath = folderPath;
-	}
+	
+	/**
+	 * Returns the count.
+	 *
+	 * @return the count
+	 */
 	public int getCount() {
 		return count;
 	}
 	
+	/**
+	 * Returns the folder path.
+	 *
+	 * @return the folder path
+	 */
+	public String getFolderPath() {
+		return folderPath;
+	}
+	
+	/**
+	 * Sets the count.
+	 *
+	 * @param count the new count
+	 */
 	private void setCount(int count) {
 		this.count = count;
 	}
 	
-	public void addOne(){
-		this.count++;
+	/**
+	 * Sets the folder path.
+	 *
+	 * @param folderPath the new folder path
+	 */
+	public void setFolderPath(String folderPath) {
+		this.folderPath = folderPath;
 	}
 
 
