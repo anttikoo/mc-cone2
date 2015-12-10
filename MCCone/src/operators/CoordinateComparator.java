5c72fcc42b42834962925c374c9a852b5edb9711
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-10 15:53:52 +0200

    commenting

--- MCCone/src/operators/CoordinateComparator.java
+++ MCCone/src/operators/CoordinateComparator.java
@@ -3,8 +3,15 @@ package operators;
 import java.awt.Point;
 import java.util.Comparator;
 
+/**
+ * The Class CoordinateComparator. Compares coordinates of Points by horizontal position and returns which is Point smaller. 
+ * If horizontal position is same then vertical position is compared.
+ */
 public class CoordinateComparator implements Comparator<Point>{
 
+	/* (non-Javadoc)
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 */
 	@Override
 	public int compare(Point p1, Point p2) {
 		// x-position is same -> compare with y position
@@ -15,12 +22,9 @@ public class CoordinateComparator implements Comparator<Point>{
 		// 
 		else{
 			if(p1.x < p2.x) // first point has smaller x 
-				return -1;
-			
+				return -1;		
 		}
 		return 1;
 	}
-
-	
 
 }
