1c9edbe232a57fbf59525c9fd5de9059064b4f2e
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-10 16:41:05 +0200

    commenting

--- MCCone/src/operators/SearchImageLayersHandler.java
+++ MCCone/src/operators/SearchImageLayersHandler.java
@@ -1,17 +1,8 @@
 package operators;
 
-import gui.ShadyMessageDialog;
-import information.ID;
-import information.ImageLayer;
-import information.MarkingLayer;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.logging.Logger;
-
-import javax.security.sasl.SaslException;
-import javax.swing.JFrame;
-
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -24,9 +15,6 @@ import org.xml.sax.helpers.DefaultHandler;
 public class SearchImageLayersHandler extends DefaultHandler {
 
 	private ArrayList<String> imageLayerNameList = null;
-	private ImageLayer singleImageLayer=null;
-	private ImageLayer selectedImageLayer=null;
-	private MarkingLayer selectedMarkingLayer=null;
 	private boolean foundImageLayer=false;
 	private final static Logger LOGGER = Logger.getLogger("MCCLogger");
 
@@ -37,6 +25,9 @@ public class SearchImageLayersHandler extends DefaultHandler {
 		this.imageLayerNameList=imageLayerNameList;
 	}
 
+    /* (non-Javadoc)
+     * @see org.xml.sax.helpers.DefaultHandler#startElement(java.lang.String, java.lang.String, java.lang.String, org.xml.sax.Attributes)
+     */
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes)
             throws SAXException {
@@ -49,11 +40,17 @@ public class SearchImageLayersHandler extends DefaultHandler {
         }
     }
 
+    /* (non-Javadoc)
+     * @see org.xml.sax.helpers.DefaultHandler#endElement(java.lang.String, java.lang.String, java.lang.String)
+     */
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
     	//do nothing
     }
 
+    /* (non-Javadoc)
+     * @see org.xml.sax.helpers.DefaultHandler#characters(char[], int, int)
+     */
     @Override
     public void characters(char ch[], int start, int length) throws SAXException {
     	// do nothing
@@ -61,6 +58,12 @@ public class SearchImageLayersHandler extends DefaultHandler {
 
 
 
+    /**
+     * Searches ImageLayer.
+     *
+     * @param i_name the name of ImageLayer
+     * @throws SAXException the SAX exception
+     */
     private void searchImageLayer(String i_name) throws SAXException{
     	try {
 			// go through imageLayerList
@@ -74,20 +77,27 @@ public class SearchImageLayersHandler extends DefaultHandler {
 					}
 				}
 			}
-
-
 		} catch (Exception e) {
 			LOGGER.severe("Error in going trough imageLayerList " +e.getClass().toString() + " :" +e.getMessage());
-			selectedImageLayer=null;
 			throw new SAXException();
 		}
 
     }
 
+	/**
+	 * Checks boolean has ImageLayer found.
+	 *
+	 * @return true, if is found image layer
+	 */
 	public boolean isFoundImageLayer() {
 		return foundImageLayer;
 	}
 
+	/**
+	 * Sets the boolean ha ImageLayer found.
+	 *
+	 * @param foundImageLayer the new found image layer
+	 */
 	public void setFoundImageLayer(boolean foundImageLayer) {
 		this.foundImageLayer = foundImageLayer;
 	}
