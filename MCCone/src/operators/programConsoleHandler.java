ceb21ca9519b345f075ed18474e9688204f5fe48
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-10 16:38:02 +0200

    comments

--- MCCone/src/operators/programConsoleHandler.java
+++ MCCone/src/operators/programConsoleHandler.java
@@ -5,12 +5,24 @@ import gui.LogFrame;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.LogRecord;
 
+/**
+ * The Class programConsoleHandler. Prints messages to log.
+ */
 public class programConsoleHandler extends ConsoleHandler{
 	private LogFrame logFrame;
+	
+	/**
+	 * Instantiates a new program console handler.
+	 *
+	 * @param logFrame the log frame
+	 */
 	public programConsoleHandler(LogFrame logFrame){
 		this.logFrame=logFrame;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.util.logging.ConsoleHandler#publish(java.util.logging.LogRecord)
+	 */
 	public void publish(LogRecord record){
 		this.logFrame.printMessage(record);
 	}
