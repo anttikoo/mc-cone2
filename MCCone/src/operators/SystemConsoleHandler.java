e3f98d0302ba4d36d5f4a70dc7afb754ba7c4415
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-11 09:52:16 +0200

    comment

--- MCCone/src/operators/SystemConsoleHandler.java
+++ MCCone/src/operators/SystemConsoleHandler.java
@@ -1,14 +1,18 @@
 package operators;
 
-import gui.LogFrame;
-
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
+/**
+ * The Class SystemConsoleHandler. Publishes the messages with System.err.println or System.out.pringln.
+ */
 public class SystemConsoleHandler extends ConsoleHandler{
 
 
+	/* (non-Javadoc)
+	 * @see java.util.logging.ConsoleHandler#publish(java.util.logging.LogRecord)
+	 */
 	public void publish(LogRecord record){
 		if(record.getLevel() == Level.SEVERE){
 			System.err.println(record.getLevel().toString()+": "+record.getMessage());
