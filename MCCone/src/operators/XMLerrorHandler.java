c21c15396f43a0939620c6e8f2805322b57a636d
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-11 10:04:52 +0200

    comments

--- MCCone/src/operators/XMLerrorHandler.java
+++ MCCone/src/operators/XMLerrorHandler.java
@@ -6,28 +6,46 @@ import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
+/**
+ * The Class XMLerrorHandler. Detects the warnings and errors in parsing xml-file 
+ * and if any error found saves information that error has been found.
+ */
 public class XMLerrorHandler implements ErrorHandler {
 	private final static Logger LOGGER = Logger.getLogger("MCCLogger");	
 	private boolean foundErrors=false;
 
-    public void warning(SAXParseException e) throws SAXException {
-    	LOGGER.warning("Warning at  " +e.getClass().toString() + " :" +e.getMessage()+ " line: " + e.getLineNumber());
-    	
-    }
-
+    /* (non-Javadoc)
+     * @see org.xml.sax.ErrorHandler#error(org.xml.sax.SAXParseException)
+     */
     public void error(SAXParseException e) throws SAXException {
     	LOGGER.severe("Error in  " +e.getClass().toString() + " :" +e.getMessage()+ " line: " + e.getLineNumber());
     	this.foundErrors=true;
     }
 
+    /* (non-Javadoc)
+     * @see org.xml.sax.ErrorHandler#fatalError(org.xml.sax.SAXParseException)
+     */
     public void fatalError(SAXParseException e) throws SAXException {
     	LOGGER.severe("Error in  " +e.getClass().toString() + " :" +e.getMessage()+ " line: " + e.getLineNumber());
     	this.foundErrors=true;
     }
 
+    /**
+	 * Checks if is errors found.
+	 *
+	 * @return true, if is errors found
+	 */
 	public boolean isErrorsFound() {
 		return foundErrors;
 	}
+
+	/* (non-Javadoc)
+     * @see org.xml.sax.ErrorHandler#warning(org.xml.sax.SAXParseException)
+     */
+    public void warning(SAXParseException e) throws SAXException {
+    	LOGGER.warning("Warning at  " +e.getClass().toString() + " :" +e.getMessage()+ " line: " + e.getLineNumber());
+    	
+    }
 
 	
 }
