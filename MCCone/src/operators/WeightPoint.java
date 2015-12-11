a46ea1e8667045b3bff2de46318813d5363df742
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-11 10:01:23 +0200

    commenting

--- MCCone/src/operators/WeightPoint.java
+++ MCCone/src/operators/WeightPoint.java
@@ -1,59 +1,109 @@
 package operators;
 
 import java.awt.Point;
-
 import math.geom2d.Point2D;
 
 
-
+/**
+ * The Class WeightPoint. Contains a weight value for point.
+ */
 public class WeightPoint extends Point{
-//	private Point point;
 	private double weight;
 
+	/**
+	 * Instantiates a new WeightPoint by given Point.
+	 *
+	 * @param point the Point
+	 */
 	public WeightPoint(Point point){
 		super(point);
 		this.setWeight(1);
 		setPoint(point);
 	}
 
+	/**
+	 * Instantiates a new WeightPoint by given WeightPoint
+	 *
+	 * @param wp the WeightPoint
+	 */
 	public WeightPoint(WeightPoint wp){
 		super(wp.getPoint());
 		this.setWeight(wp.getWeight());
 		//setPoint(point);
 	}
 
-	public void increaseWeight(double increase){
-		this.setWeight(this.getWeight() + increase);
+	/**
+	 * Decreases weight by dividing with given double value.
+	 *
+	 * @param d the double weight
+	 */
+	public void decreaseWeight(double d){
+		this.weight=this.weight/d;
 	}
 
-	public Point getPoint() {
-		return this;
-	}
-
-	public Point2D getPoint2D() {
-		return new Point2D(this.x, this.y);
-	}
-
-	public void setPoint(Point p) {
-	//	this.point = point;
-		this.x=p.x;
-		this.y=p.y;
-	}
-
-	public double getWeight() {
-		return weight;
-	}
-
-	public void setWeight(double weight) {
-		this.weight = weight;
-	}
-
+	/**
+	 * Distance.
+	 *
+	 * @param p2 the p2
+	 * @return the double
+	 */
 	public double distance(WeightPoint p2){
 
 		return this.getPoint().distance(p2.getPoint());
 	}
 
-	public void decreaseWeight(double d){
-		this.weight=this.weight/d;
+	/**
+	 * Returns the point.
+	 *
+	 * @return the Point
+	 */
+	public Point getPoint() {
+		return this;
+	}
+
+	/**
+	 * Returns the point2 d.
+	 *
+	 * @return the point2 d
+	 */
+	public Point2D getPoint2D() {
+		return new Point2D(this.x, this.y);
+	}
+
+	/**
+	 * Returns the weight.
+	 *
+	 * @return the double weight
+	 */
+	public double getWeight() {
+		return weight;
+	}
+
+	/**
+	 * Increases weight by summing with given double value.
+	 *
+	 * @param increase the increase
+	 */
+	public void increaseWeight(double increase){
+		this.setWeight(this.getWeight() + increase);
+	}
+
+	/**
+	 * Sets the point.
+	 *
+	 * @param p the new point
+	 */
+	public void setPoint(Point p) {
+		this.x=p.x;
+		this.y=p.y;
+	}
+
+	/**
+	 * Sets the weight.
+	 *
+	 * @param weight the new weight
+	 */
+	public void setWeight(double weight) {
+		this.weight = weight;
 	}
 }
