70e1ea34a7ba54543668a51cbae9cbe1058f3642
Antti Kurronen <antti.kurronen@gmail.com>
2015-12-11 10:59:04 +0200

    ordered

--- MCCone/src/operators/XMLtags.java
+++ MCCone/src/operators/XMLtags.java
@@ -2,48 +2,47 @@ package operators;
 
 import javax.xml.namespace.QName;
 
+/**
+ * The Class XMLtags. Static fields for parsing xml-file.
+ */
 public class XMLtags {
 
-	public static final String layers ="layers";
-	public static final String imagelayer ="imagelayer";
-	public static final String markinglayer ="markinglayer";
-	public static final String markingname ="markingname";
-	public static final String imagename ="imagename";
-	public static final String coordinates ="coordinates";
 	public static final String color ="color";
-	public static final String singlecoordinate ="singlecoordinate";
-	public static final String shape ="shape";
-	public static final String opacity ="opacity";
-	public static final String thickness ="thickness";
-	public static final String size ="size";
+	public static final String column ="column";
+	public static final String coordinates ="coordinates";
 	public static final String grid ="grid";
 	public static final String grid_on ="grid_on";
-	public static final String lines="lines";
-	public static final String rectangles ="rectangles";
-	public static final String x ="x";
-	public static final String y ="y";
-	public static final String rec ="rec";
-//	public static final String selected_rec ="selected_rec";
-//	public static final String unselected_rec ="unselected_rec";
-//	public static final String unselected_rec_numbers ="unselected_rec_numbers";
-//	public static final String num ="num";
-	public static final String selected ="selected";
-	public static final String column ="column";
-	public static final String row ="row";
-	public static final String width ="width";
 	public static final String height ="height";
-	public static final String value_true ="true";
-	public static final String value_false ="false";
-	public static final QName qnameLayers=new QName(layers);
+	public static final String imagelayer ="imagelayer";
+	public static final String imagename ="imagename";
+	public static final String layers ="layers";
+	public static final String lines="lines";
+	public static final String markinglayer ="markinglayer";
+	public static final String markingname ="markingname";
+	public static final String opacity ="opacity";
+	public static final QName qnameColor=new QName(color);
+	public static final QName qnameCoordinates=new QName(coordinates);
 	public static final QName qnameImageLayer=new QName(imagelayer);
 	public static final QName qnameImageName=new QName(imagename);
-	public static final QName qnameMarkingName=new QName(markingname);
+	public static final QName qnameLayers=new QName(layers);
 	public static final QName qnameMarkingLayer=new QName(markinglayer);
-	public static final QName qnameCoordinates=new QName(coordinates);
-	public static final QName qnameColor=new QName(color);
-	public static final QName qnameSingleCoordinate=new QName(singlecoordinate);
-	public static final QName qnameShape=new QName(shape);
+	public static final QName qnameMarkingName=new QName(markingname);
 	public static final QName qnameOpacity=new QName(opacity);
-	public static final QName qnameThickness=new QName(thickness);
+	public static final String shape ="shape";
+	public static final QName qnameShape=new QName(shape);
+	public static final String singlecoordinate ="singlecoordinate";
+	public static final QName qnameSingleCoordinate=new QName(singlecoordinate);
+	public static final String size ="size";
 	public static final QName qnameSize=new QName(size);
+	public static final String thickness ="thickness";
+	public static final QName qnameThickness=new QName(thickness);
+	public static final String rec ="rec";
+	public static final String rectangles ="rectangles";
+	public static final String row ="row";
+	public static final String selected ="selected";	
+	public static final String value_false ="false";
+	public static final String value_true ="true";
+	public static final String width ="width";
+	public static final String x ="x";
+	public static final String y ="y";
 }
