45ba0dcee3960d97c90be338341defb5425292ce
Antti Kurronen <antti.kurronen@gmail.com>
2016-02-09 10:41:25 +0200

    try catches

--- MCCone/src/gui/graphics/BigCloseIcon.java
+++ MCCone/src/gui/graphics/BigCloseIcon.java
@@ -7,6 +7,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.net.URL;
+import java.util.logging.Logger;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
@@ -14,6 +16,9 @@ import javax.swing.ImageIcon;
  * The Class BigCloseIcon. Icon for close buttons. Size 18px.
  */
 public class BigCloseIcon implements Icon {
+	
+	/** The Constant LOGGER. */
+	private final static Logger LOGGER = Logger.getLogger("MCCLogger");
 	
 	/** The selected. Is Icon selected.*/
 	private boolean selected=false;
@@ -32,15 +37,23 @@ public class BigCloseIcon implements Icon {
   	 */
   	public void paintIcon(Component component, Graphics g, int x, int y) {
 		  x =0;y =0;
-	
-	    Graphics2D g2d = (Graphics2D)g.create();
-	    g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC, 1.0f));
-	    if(this.selected)
-	        g2d.drawImage(createImage("/images/close_25_selected.png","info"), x, y, component);
-	    else
-	        g2d.drawImage(createImage("/images/close_25.png","info"), x, y, component);
+		  Graphics2D g2d = null;
+		  
+	    try {
+			g2d = (Graphics2D)g.create();
+			g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC, 1.0f));
+			if(this.selected)
+			    g2d.drawImage(createImage("/images/close_25_selected.png","info"), x, y, component);
+			else
+			    g2d.drawImage(createImage("/images/close_25.png","info"), x, y, component);
 
-	    g2d.dispose();
+			g2d.dispose();
+		} catch (Exception e) {
+			LOGGER.severe("Error in painting big icon!");
+			e.printStackTrace();
+			if(g2d != null)
+				g2d.dispose();
+		}
 	  }
 	  
   	/* (non-Javadoc)
@@ -65,22 +78,28 @@ public class BigCloseIcon implements Icon {
   	 * @return the image
   	 */
   	private Image createImage(String path, String description) {
-	        URL imageURL = BigCloseIcon.class.getResource(path);
-	        Image icn = null;
+	        try {
+				URL imageURL = BigCloseIcon.class.getResource(path);
+				Image icn = null;
 
 
-	        if (imageURL == null) {
-	            if(icn==null){
-	              
-	                icn = new ImageIcon (BigCloseIcon.class.getResource(path.replace("..",""))).getImage();
-	                if(icn != null)
-	                    return icn;
+				if (imageURL == null) {
+				    if(icn==null){
+				      
+				        icn = new ImageIcon (BigCloseIcon.class.getResource(path.replace("..",""))).getImage();
+				        if(icn != null)
+				            return icn;
 
-	            }
-	             return null;
-	        } else {
-	            return (new ImageIcon(imageURL, description)).getImage();
-	        }
+				    }
+				     return null;
+				} else {
+				    return (new ImageIcon(imageURL, description)).getImage();
+				}
+			} catch (Exception e) {
+				LOGGER.severe("Error in creating image from path!");
+				e.printStackTrace();
+				return null;
+			}
 	    }
 
 	}
--- MCCone/src/gui/graphics/SmallCloseIcon.java
+++ MCCone/src/gui/graphics/SmallCloseIcon.java
@@ -7,6 +7,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.net.URL;
+import java.util.logging.Logger;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
@@ -14,6 +16,9 @@ import javax.swing.ImageIcon;
  * The Class SmallCloseIcon. Icon for close button. Size 16px.
  */
 public class SmallCloseIcon implements Icon {
+	
+	/** The Constant LOGGER. */
+	private final static Logger LOGGER = Logger.getLogger("MCCLogger");
 	
 	/** The selected. Is Icon selected. */
 	private boolean selected=false;
@@ -32,14 +37,23 @@ public class SmallCloseIcon implements Icon {
   	 */
   	public void paintIcon(Component component, Graphics g, int x, int y) {
 		  x =0;y =0;
-	    Graphics2D g2d = (Graphics2D)g.create();
-	    g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC, 1.0f));
-	    if(this.selected)
-	        g2d.drawImage(createImage("/images/close_small_selected.png","info"), x, y, component);
-	    else
-	        g2d.drawImage(createImage("/images/close_small.png","info"), x, y, component);
+		  Graphics2D g2d = null;
+	    try {
+			g2d = (Graphics2D)g.create();
+			g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC, 1.0f));
+			if(this.selected)
+			    g2d.drawImage(createImage("/images/close_small_selected.png","info"), x, y, component);
+			else
+			    g2d.drawImage(createImage("/images/close_small.png","info"), x, y, component);
 
-	    g2d.dispose();
+			g2d.dispose();
+		} catch (Exception e) {
+			LOGGER.severe("Error in painting smal icon!");
+			e.printStackTrace();
+			if(g2d != null)
+				g2d.dispose();
+			
+		}
 	  }
 	  
   	/* (non-Javadoc)
@@ -64,22 +78,28 @@ public class SmallCloseIcon implements Icon {
   	 * @return the image
   	 */
   	private Image createImage(String path, String description) {
-	        URL imageURL = SmallCloseIcon.class.getResource(path);
-	        Image icn = null;
+	        try {
+				URL imageURL = SmallCloseIcon.class.getResource(path);
+				Image icn = null;
 
 
-	        if (imageURL == null) {
-	            if(icn==null){
-	            
-	                icn = new ImageIcon (SmallCloseIcon.class.getResource(path.replace("..",""))).getImage();
-	                if(icn != null)
-	                    return icn;
+				if (imageURL == null) {
+				    if(icn==null){
+				    
+				        icn = new ImageIcon (SmallCloseIcon.class.getResource(path.replace("..",""))).getImage();
+				        if(icn != null)
+				            return icn;
 
-	            }
-	             return null;
-	        } else {
-	            return (new ImageIcon(imageURL, description)).getImage();
-	        }
+				    }
+				     return null;
+				} else {
+				    return (new ImageIcon(imageURL, description)).getImage();
+				}
+			} catch (Exception e) {
+				LOGGER.severe("Error in creating  image for small icon!");
+				e.printStackTrace();
+				return null;
+			}
 	    }
 
 	}
